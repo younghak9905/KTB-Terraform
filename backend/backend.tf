0962e5cccfa60cc2489aedc6790ee5100fc2617a
younghak9905 <yeonghak9905@gmail.com>
2025-03-19 17:03:51 +0900

    backend

---
+++ backend/backend.tf
@@ -0,0 +1,64 @@
+resource "aws_s3_bucket" "terraform_state" { 
+  bucket = "zero9905-terraformstate"
+  force_destroy = false
+}
+
+resource "aws_s3_bucket_versioning" "enabled" { 
+  bucket = aws_s3_bucket.terraform_state.id 
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" { 
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.terraform_state.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "terraform_state_policy" {
+  bucket = aws_s3_bucket.terraform_state.id
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": [
+        "s3:GetBucketPolicy",
+        "s3:ListBucket",
+        "s3:GetObject",
+        "s3:PutObject"
+      ],
+      "Resource": [
+        "arn:aws:s3:::zero9905-terraformstate",
+        "arn:aws:s3:::zero9905-terraformstate/*"
+      ]
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "zero9905-terraformstate"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
