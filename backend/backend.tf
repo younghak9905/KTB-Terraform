6f988c5e3c079833b25ab9751d58f482018392bc
younghak9905 <yeonghak9905@gmail.com>
2025-03-20 13:44:10 +0900

    ws_dynamodb_table

--- backend/backend.tf
+++ backend/backend.tf
@@ -71,7 +71,7 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 
 # DynamoDB 테이블: create_backend_resources가 true일 때만 생성합니다.
 resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "zero9905-terraform-state"
+  name         = "zero9905-terraformstate"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
