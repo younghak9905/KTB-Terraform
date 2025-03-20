646b266c17b13fb7da8373fec494a955ccc04b6c
younghak9905 <yeonghak9905@gmail.com>
2025-03-20 10:00:21 +0900

    iam 수정

--- terraform_stage/iam.tf
+++ terraform_stage/iam.tf
@@ -1,14 +1,7 @@
 module "iam_service_role" {
-  source      = "./modules/iam/iam-service-role"
+  source      = "../modules/iam/iam-service-role"
   stage       = var.stage
   servicename = var.servicename
   tags        = var.tags  
-}
-
-module "iam_backend" {
-  source          = "../modules/iam/iam-service-role"
-  stage       = var.stage
-  servicename = var.servicename
-  tags        = var.tags  
-  aws_account_id  = var.aws_account_id
+  aws_account_id = var.aws_account_id
 }
