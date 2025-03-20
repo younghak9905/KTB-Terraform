d43a4847433001e2c608e96f01534a8eb1715354
younghak9905 <yeonghak9905@gmail.com>
2025-03-20 10:07:46 +0900

    dynamodb 없으면 생성 하도록 수정

--- backend/backend.tf
+++ backend/backend.tf
@@ -71,13 +71,16 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 
 # DynamoDB 테이블: create_backend_resources가 true일 때만 생성합니다.
 resource "aws_dynamodb_table" "terraform_lock" {
-  count       = var.create_backend_resources ? 1 : 0
-  name         = "zero9905-terraformstate"
+  name         = "zero9905-terraform-state"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
   attribute {
     name = "LockID"
     type = "S"
+  }
+
+  lifecycle {
+    prevent_destroy = true  # 삭제 방지
   }
 }
