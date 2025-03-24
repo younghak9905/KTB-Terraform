6fbbcbb44fc3e7dbe4d732e7504260ef38e8f926
younghak9905 <yeonghak9905@gmail.com>
2025-03-24 13:33:37 +0900

    중복 방지 테스트

--- terraform_stage/main.tf
+++ terraform_stage/main.tf
@@ -135,7 +135,7 @@ module "alb" {
   sg_allow_comm_list = ["0.0.0.0/0"]  # 필요 시 수정
 
 }
-
+/*
 module "asg" {
   source                      = "../modules/asg"
   asg_name                    = "my_ecs_asg"
@@ -183,7 +183,7 @@ module "ecs_ec2" {
 
 }
 
-
+*/
 #RDS
 # module "rds" {
 #   #default engin aurora-mysql8.0
