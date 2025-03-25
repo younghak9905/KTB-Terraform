d963119b403d77b65a177e23a7246fe64525a3c6
younghak9905 <yeonghak9905@gmail.com>
2025-03-25 11:30:20 +0900

    모듈에 ssm 정책 추가

--- modules/iam/iam-service-role/role.tf
+++ modules/iam/iam-service-role/role.tf
@@ -24,6 +24,64 @@ resource "aws_iam_instance_profile" "ec2-iam-role-profile" {
   role = aws_iam_role.ec2-iam-role.name
 }
 
+# modules/iam/iam-service-role/policy.tf 생성 (파일이 없는 경우)
+
+# SSM 접근을 위한 정책 생성
+resource "aws_iam_policy" "ssm_policy" {
+  name        = "aws-iam-${var.stage}-${var.servicename}-ssm-policy"
+  description = "Policy for SSM access and ECS instance management"
+  
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "ssm:UpdateInstanceInformation",
+          "ssm:ListInstanceAssociations",
+          "ssm:DescribeInstanceProperties",
+          "ssm:DescribeDocumentParameters",
+          "ssmmessages:CreateControlChannel",
+          "ssmmessages:CreateDataChannel",
+          "ssmmessages:OpenControlChannel",
+          "ssmmessages:OpenDataChannel",
+          "ec2messages:AcknowledgeMessage",
+          "ec2messages:DeleteMessage",
+          "ec2messages:FailMessage",
+          "ec2messages:GetEndpoint",
+          "ec2messages:GetMessages",
+          "ec2messages:SendReply"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      },
+      {
+        Action = [
+          "ecs:ListClusters",
+          "ecs:ListContainerInstances",
+          "ecs:DescribeContainerInstances",
+          "ec2:DescribeInstances"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      }
+    ]
+  })
+  
+  tags = var.tags
+}
+
+# EC2 IAM 역할에 SSM 정책 연결
+resource "aws_iam_role_policy_attachment" "ec2_ssm_policy_attachment" {
+  role       = aws_iam_role.ec2-iam-role.name
+  policy_arn = aws_iam_policy.ssm_policy.arn
+}
+
+# EC2 IAM 역할에 기본 Amazon EC2 컨테이너 서비스 정책 연결
+resource "aws_iam_role_policy_attachment" "ec2_ecs_policy_attachment" {
+  role       = aws_iam_role.ec2-iam-role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
 /*resource "aws_iam_role" "terraform_backend_role" {
   name = "TerraformS3Role"
 
